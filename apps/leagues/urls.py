037d34e3ae8924747af0a7088f35cb9e55efd952
Wolf Elkan <wolf.elkan@gmail.com>
2017-02-25 15:01:12 -0500

    All queries added

--- apps/leagues/urls.py
+++ apps/leagues/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	url(r"^$", views.index, name="index"),
 	url(r"^make_data/", views.make_data, name="make_data"),
-	url(r"^(?P<level>[1-3])/(?P<question>\d+)$", views.levels),
+	url(r"^(?P<level>[1-3])/(?P<question>1?\d)$", views.levels),
 ]
--- apps/leagues/views.py
+++ apps/leagues/views.py
@@ -1,7 +1,22 @@
+# - - - - - IMPORTS - - - - -
+
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
 
 from . import team_maker
+from django.db.models import Q, Count
+
+# - - - - - HELPER FUNCTIONS - - - - -
+
+def team_id(location,team_name):
+	query = Q(location=location)&Q(team_name=team_name)
+	return Team.objects.get(query).id
+
+def player_id(first_name,last_name):
+	query = Q(first_name=first_name)&Q(last_name=last_name)
+	return Player.objects.get(query).id	
+
+# - - - - - VIEWS - - - - -
 
 def index(request):
 	context = {
@@ -17,6 +32,8 @@ def make_data(request):
 	team_maker.gen_players(200)
 
 	return redirect("index")
+
+# - - - - - QUESTION DIRECTORY - - - - -
 
 def levels(request, level, question):
 	if level == "1":
@@ -110,129 +127,292 @@ def L3(request, question):
 
 # 1. Find all baseball leagues 
 def L1Q1(request):
-	pass
+	context = {
+		"leagues": League.objects.filter(sport="Baseball"),
+		"teams": [" "],
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 2. Find all womens' leagues 
 def L1Q2(request):
-	pass
+	context = {
+		"leagues": League.objects.filter(name__icontains="women"),
+		"teams": [" "],
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 3. Find all leagues where sport is any type of hockey 
 def L1Q3(request):
-	pass
+	context = {
+		"leagues": League.objects.filter(name__icontains="hockey"),
+		"teams": [" "],
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 4. Find all leagues where sport is something OTHER THAN football 
 def L1Q4(request):
-	pass
+	context = {
+		"leagues": League.objects.exclude(name__icontains="football"),
+		"teams": [" "],
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 5. Find all leagues that call themselves "conferences" 
 def L1Q5(request):
-	pass
+	context = {
+		"leagues": League.objects.filter(name__contains="Conference"),
+		"teams": [" "],
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 6. Find all leagues in the Atlantic region 
 def L1Q6(request):
-	pass
+	context = {
+		"leagues": League.objects.filter(name__contains="Atlantic"),
+		"teams": [" "],
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 7. Find all teams based in Dallas 
 def L1Q7(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.filter(location="Dallas"),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 8. Find all teams named the Raptors 
 def L1Q8(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.filter(team_name="Raptors"),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 9. Find all teams whose location includes "City" 
 def L1Q9(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.filter(location__icontains="city"),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 10. Find all teams whose names begin with "T" 
 def L1Q10(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.filter(team_name__startswith="T"),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 11. Return all teams, ordered alphabetically by location 
 def L1Q11(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.order_by('location'),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 12. Return all teams, ordered by team name in reverse alphabetical order 
 def L1Q12(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.order_by('team_name').reverse(),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 13. Find every player with last name "Cooper" 
 def L1Q13(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(last_name="Cooper"),
+	}
+	return render(request, "leagues/index.html", context)
 
 # 14. Find every player with first name "Joshua" 
 def L1Q14(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(first_name="Joshua"),
+	}
+	return render(request, "leagues/index.html", context)
 
 # 15. Find every player with last name "Cooper" EXCEPT FOR Joshua 
 def L1Q15(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(last_name="Cooper").exclude(first_name="Joshua"),
+	}
+	return render(request, "leagues/index.html", context)
+
 
 # 16. Find all players with first name "Alexander" OR first name "Wyatt" 
 def L1Q16(request):
-	pass
+	player_query = Q(first_name="Alexander")|Q(first_name="Wyatt")
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(player_query),
+	}
+	return render(request, "leagues/index.html", context)
 
 # - - - - - LEVEL 2 - - - - -
 
 # 1. Find all teams in the Atlantic Soccer Conference 
 def L2Q1(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.filter(league__name="Atlantic Soccer Conference"),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 2. Find all (current) players on the Boston Penguins 
 def L2Q2(request):
-	pass
+	player_query = Q(curr_team__location="Boston")|Q(curr_team__team_name="Penguins")
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(player_query)
+	}
+	return render(request, "leagues/index.html", context)
 
 # 3. Find all (current) players in the International Collegiate Baseball Conference 
 def L2Q3(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(curr_team__league__name="International Collegiate Baseball Conference")
+	}
+	return render(request, "leagues/index.html", context)
 
 # 4. Find all (current) players in the American Conference of Amateur Football with last name "Lopez" 
 def L2Q4(request):
-	pass
+	player_query = Q(curr_team__league__name="American Conference of Amateur Football")&Q(last_name="Lopez")
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(player_query)
+	}
+	return render(request, "leagues/index.html", context)
 
 # 5. Find all football players 
 def L2Q5(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(curr_team__league__name__icontains="football")
+	}
+	return render(request, "leagues/index.html", context)
 
 # 6. Find all teams with a (current) player named "Sophia" 
 def L2Q6(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.filter(curr_players__first_name="Sophia"),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 7. Find all leagues with a (current) player named "Sophia" 
 def L2Q7(request):
-	pass
+	context = {
+		"leagues": League.objects.filter(teams__curr_players__first_name="Sophia"),
+		"teams": [" "],
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 8. Find everyone with the last name "Flores" who DOESN'T (currently) play for the Washington Roughriders 
 def L2Q8(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(last_name="Flores")
+		.exclude(curr_team__id=team_id("Washington","Roughriders"))
+	}
+	return render(request, "leagues/index.html", context)
 
 # - - - - - LEVEL 3 - - - - - 
 
 # 1. Find all teams, past and present, that Samuel Evans has played with 
 def L3Q1(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.filter(all_players__id=player_id("Samuel","Evans")),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 2. Find all players, past and present, with the Manitoba Tiger-Cats 
 def L3Q2(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(all_teams__id=team_id("Manitoba","Tiger-Cats")),
+	}
+	return render(request, "leagues/index.html", context)
 
-# 3. Find all players who were formerly (but aren't currently) with the Wichita Vikings 
+# 3. Find all players who were formerly (but aren't currently) with the Wichita Vikings
 def L3Q3(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(all_teams__id=team_id("Wichita","Vikings"))
+		.exclude(curr_team__id=team_id("Wichita","Vikings"))
+	}
+	return render(request, "leagues/index.html", context)
 
 # 4. Find every team that Jacob Gray played for before he joined the Oregon Colts 
 def L3Q4(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.filter(all_players__id=player_id("Jacob","Gray"))
+		.exclude(id=team_id("Oregon","Colts")),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 5. Find everyone named "Joshua" who has ever played in the Atlantic Federation of Amateur Baseball Players 
 def L3Q5(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.filter(first_name="Joshua")
+		.filter(all_teams__league__name="Atlantic Federation of Amateur Baseball Players")
+	}
+	return render(request, "leagues/index.html", context)
 
 # 6. Find all teams that have had 12 or more players, past and present.  
 # (HINT: Look up the Django `annotate` function.) 
 def L3Q6(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": Team.objects.annotate(nPlayers=Count('all_players')).filter(nPlayers__gte=12),
+		"players": [" "],
+	}
+	return render(request, "leagues/index.html", context)
 
 # 7. Show all players, sorted by the number of teams they've played for 
 def L3Q7(request):
-	pass
+	context = {
+		"leagues": [" "],
+		"teams": [" "],
+		"players": Player.objects.annotate(nTeams=Count('all_teams')).order_by('nTeams')
+	}
+	return render(request, "leagues/index.html", context)
